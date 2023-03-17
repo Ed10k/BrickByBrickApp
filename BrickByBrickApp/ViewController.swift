4bc6f231d14599510f44f648503e377d2d378aaf
Joe Edozie <Josephedozie45@gmail.com>
2023-03-17 12:15:28 -0500

    Initial Commit

---
+++ BrickByBrickApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrickByBrickApp
+//
+//  Created by Joseph Edozie on 3/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BrickByBrickAppUITests/BrickByBrickAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BrickByBrickAppUITestsLaunchTests.swift
+//  BrickByBrickAppUITests
+//
+//  Created by Joseph Edozie on 3/17/23.
+//
+
+import XCTest
+
+final class BrickByBrickAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
